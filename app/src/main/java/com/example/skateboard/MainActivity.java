37a6d928de41a3c816f5ef88e6d3026b8e6bf45d
unknown <cgullickson2@wisc.edu>
2018-10-21 11:54:58 -0500

    Login and sample fragment

--- app/src/main/java/com/example/skateboard/MainActivity.java
+++ app/src/main/java/com/example/skateboard/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.skateboard;
 
+import android.content.res.Resources;
 import android.databinding.DataBindingUtil;
 import android.databinding.Observable;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,7 +36,18 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setMainActivity(this);
 
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        home fragment = new home();
+//        fragmentTransaction.add(R.id.textView, fragment);
+//        fragmentTransaction.commit();
+
         databaseRepository.signIn("gruen065@umn.edu", "redwire15");
+
+        //Set username for this profile
+//        String firstName = databaseRepository.getUser().get().getFirstName();
+//        Resources res = getResources();
+//        String format = String.format(res.getString(R.string.userName), firstName);
 
         databaseRepository.getUser().addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
             @Override
